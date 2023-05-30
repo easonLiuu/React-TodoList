3f502c153c803a4d26b9a6570aa36184c6fc5c44
liujiarui <ljr3289595084@163.com>
2023-05-30 20:56:48 +0800

    feat: add mobx demos

---
+++ 5-Mobx/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BasicDemo from "./pages/BasicDemo";
+
+function App() {
+  return (
+    <>
+      <h1>Mobx Demo</h1>
+      <BasicDemo />
+    </>
+  );
+}
+
+export default App;
